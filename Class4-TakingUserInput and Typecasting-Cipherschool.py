29f0b1ca6007bc6a3e22cee6df4338811153a082
Mohammad Amaanul Hoda <119109275+Amaanulhoda@users.noreply.github.com>
2022-12-01 21:58:03 +0530

    Create Class4-TakingUserInput and Typecasting-Cipherschool.py

---
+++ Class4-TakingUserInput and Typecasting-Cipherschool.py
@@ -0,0 +1,15 @@
+# taking input from user
+d = input()
+print(d,type(d))
+e = input()
+print(e,type(e))
+
+
+
+#typecasting
+c = 15
+print(type(c))
+c = str(c)
+print(type(c))
+c = float(c)
+print(type(c))
